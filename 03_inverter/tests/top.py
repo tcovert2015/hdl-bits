6adf828407e6d8a2833b130f9475364b390c97e8
Travis Covert <tcovert2015@gmail.com>
2020-08-29 15:13:22 -0500

    Created first random test

--- 03_inverter/tests/top.py
+++ 03_inverter/tests/top.py
@@ -1,19 +1,19 @@
 import cocotb
 from cocotb.triggers import Timer
 from inverter_model import inverter_model
+import random
 
 # This test is really bad. Just learning cocotb and python...
 # Going to attempt to increase the sophistication in this example.
 
+###############################
+# Test 1 - Change Inputs to 1 #
+###############################
 @cocotb.test()
 async def inverter(dut):
     """Try accessing the design."""
 
     output_z = str(dut.output_z)
-    
-    ###############################
-    # Test 1 - Change Inputs to 1 #
-    ###############################
     
     dut.input_a <= 1
 
@@ -32,13 +32,37 @@ async def inverter(dut):
     else:
         dut._log.info("Test 1 Pass - Values are expected as 0.")
 
-
+####################################################
+# Test 2. Verify the design against a python model #
+####################################################
 @cocotb.test()
 async def inverter_using_model(dut):
     """ Test simple inverter """
 
     dut.input_a <= 1
 
-    await Timer(2, units='ns')
+    await Timer(1, units='ns')
 
     assert dut.output_z.value == inverter_model(1), "Inverter result is incorrect: {} != 0".format(dut.output_z.value)
+
+    dut.input_a <= 0
+
+    await Timer(1, units='ns')
+
+    assert dut.output_z.value == inverter_model(0), "Inverter result is incorrect: {} != 1".format(dut.output_z.value)
+
+####################################
+# Test 3. Introduce Randomization. #
+####################################
+@cocotb.test()
+async def random_inverter(dut):
+    """ Test simple inverter with random bits """
+
+    for i in range (100):
+        a = random.randint(0,1)
+
+        dut.input_a <= a
+
+        await Timer(2, units='ns')
+
+        assert dut.output_z.value == inverter_model(a), "Inverter result is incorrect: {} != 1".format(dut.output_z.value)
