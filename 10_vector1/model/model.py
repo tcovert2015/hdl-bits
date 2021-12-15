9bb7d2db00a29eb9267b3c634da13267c1360ee5
Travis Covert <tcovert2015@gmail.com>
2021-12-15 14:52:05 -0600

    Completed vector1

--- 10_vector1/model/model.py
+++ 10_vector1/model/model.py
@@ -1,9 +1,10 @@
+# No need for a model for this design
 def model(a: int):
 
-    outv = a
-    o0 = a[0]
-    o1 = a[1]
-    o2 = a[2]
+    # Convert a to bin with 16 bit width. Pad with zeros.
+    a = int(a)
+    a_binary = f'{a:016b}'
+    out_hi = a_binary[0:8]
+    out_lo = a_binary[8:16]
 
-
-    return outv, o0, o1, o2
+    return out_hi, out_lo
--- 10_vector1/tests/top.py
+++ 10_vector1/tests/top.py
@@ -1,5 +1,6 @@
 import cocotb
 from cocotb.triggers import Timer
+from cocotb.binary import BinaryValue
 from model import model
 import random
 
@@ -16,38 +17,52 @@ async def random_wires(dut):
         # rpdb.set_trace()
         
         # Create random values to send to model and dut
-        random_binary_vector = rand_key(3)
-
+        # Random int value with max 16 bits. unsigned.
+        random_dec_value = random.randint(0,65536)
+        #print(random_dec_value)
+#        random_binary_vector = rand_key(16)
+#        print(random_binary_vector)
+        
         # Send random values to model
-        outv, o0, o1, o2 = model(random_binary_vector)
-        outv = int(outv, base=2)
+        # out_hi, out_lo = model(random_binary_vector)
+        # print(out_hi, type(out_hi))
+        
+        # Convert random decimal to bin
+        a_binary = f'{random_dec_value:016b}'
+        a_bin_cocotb = BinaryValue(n_bits=16)
+        a_bin_cocotb.binstr = a_binary
+        #print(a_bin_cocotb, type(a_bin_cocotb))
+        #print("a_binary", a_binary, type(a_binary))
+        a_bin_cocotb_hi = str(a_bin_cocotb[0:7])
+        a_bin_cocotb_lo = str(a_bin_cocotb[8:15])
+        #print(a_bin_cocotb_hi)
+        #print(a_bin_cocotb_lo)
+        
+#        print("out_hi", out_hi, type(out_hi))
+#        out_hi = int(out_hi, base=2)
+#        out_lo = int(out_lo, base=2)
 
         # Send random values to dut
-        dut.vec <= int(random_binary_vector)
+        dut.vec <= int(random_dec_value)
 
         # Wait for dut to do its thing
         await Timer(1, units='ns')
-        dut_out = dut.outv.value
+        dut_out_hi = BinaryValue(n_bits=16)
+        dut_out_hi = dut.out_hi.value
+        dut_out_lo = BinaryValue(n_bits=16)
+        dut_out_lo = dut.out_lo.value
 
-        print("-------------------------------")
-        print("Test                           ", i)
-        print("Model Output    outv           ", bin(outv))
-        print("DUT Output      dut.outv.value ", bin(dut.outv.value))
-        print("-------------------------------\n")
+        print("---------------------------------")
+        print("Test                             ", i)
+        print("Input value                      ", a_binary)
+        print("DUT Output      dut.out_hi.value ", dut.out_hi.value)
+        print("DUT Output      dut.out_lo.value         ", dut.out_lo.value)
+        print("---------------------------------\n")
         
         # Compare results
-        if int(dut.outv.value) != int(outv):
+        if str(dut.out_hi.value) != a_bin_cocotb_hi:
             raise cocotb.result.TestFailure("DUT recorded %d value but model recorded %d" % (
-                int(dut.outv.value), int(outv)))        
-
-
-# Function to create a binary string
-def rand_key(p):
-    key1 = ""
-
-    for i in range(p):
-
-        temp = str(random.randint(0,1))
-        key1 += temp
-
-    return(key1)
+                int(dut.out_hi.value), a_bin_cocotb_hi))        
+        if str(dut.out_lo.value) != a_bin_cocotb_lo:
+            raise cocotb.result.TestFailure("DUT recorded %d value but model recorded %d" % (
+                int(dut.out_lo.value), a_bin_cocotb_lo))
